10305e4281a1bd2fc22f7485ace9de51a124ff5e
Bruno9519 <bruxo9519@gmail.com>
2025-07-21 19:42:14 +0200

    feat: Update código

--- lib/core/domain/entity/user_entity.dart
+++ lib/core/domain/entity/user_entity.dart
@@ -11,23 +11,30 @@ class UserEntity {
   UserWeightGoalEntity goal;
   UserPALEntity pal;
 
-  UserEntity(
-      {required this.birthday,
-      required this.heightCM,
-      required this.weightKG,
-      required this.gender,
-      required this.goal,
-      required this.pal});
+  UserEntity({
+    required this.birthday,
+    required this.heightCM,
+    required this.weightKG,
+    required this.gender,
+    required this.goal,
+    required this.pal,
+  });
 
   factory UserEntity.fromUserDBO(UserDBO userDBO) {
     return UserEntity(
-        birthday: userDBO.birthday,
-        heightCM: userDBO.heightCM,
-        weightKG: userDBO.weightKG,
-        gender: UserGenderEntity.fromUserGenderDBO(userDBO.gender),
-        goal: UserWeightGoalEntity.fromUserWeightGoalDBO(userDBO.goal),
-        pal: UserPALEntity.fromUserPALDBO(userDBO.pal));
+      birthday: userDBO.birthday,
+      heightCM: userDBO.heightCM,
+      weightKG: userDBO.weightKG,
+      gender: UserGenderEntity.fromUserGenderDBO(userDBO.gender),
+      goal: UserWeightGoalEntity.fromUserWeightGoalDBO(userDBO.goal),
+      pal: UserPALEntity.fromUserPALDBO(userDBO.pal),
+    );
   }
 
-  int get age => DateTime.now().difference(birthday).inDays~/365;
+  int get age => DateTime.now().difference(birthday).inDays ~/ 365;
+
+  /// Getters adicionais para compatibilidade com código legado ou adaptado
+  double get height => heightCM;
+  double get weight => weightKG;
+  UserPALEntity get activityLevel => pal;
 }
